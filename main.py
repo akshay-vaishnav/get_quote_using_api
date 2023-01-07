a009d4a6870d242344bbf5a00813d94b1b7137d3
akshay-vaishnav <akshay.vaishnav27@gmail.com>
2023-01-07 00:33:43 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#here we are creating a program in which we get quote using api when we click on kanye_button.
+
+from tkinter import *
+import requests
+
+
+def get_quote():
+    response = requests.get(url="https://api.kanye.rest") #get this api link from https://kanye.rest/
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="images/background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="", width=250, font=("Arial", 20, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="images/kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+get_quote()
+
+window.mainloop()
